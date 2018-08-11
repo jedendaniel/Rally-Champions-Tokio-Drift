78b2d514b76f441474392336b9fe693ae0e3a29f
Daniel Koźlik <jedendaniel@gmail.com>
2018-08-11 23:44:09 +0200

    Create project, set up scene, add car script

---
+++ Rally champions Tokio drift/Assets/Scripts/Car.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour {
+
+    bool up = false;
+    Rigidbody rb;
+
+	void Start () {
+        rb = gameObject.GetComponent<Rigidbody>();
+	}
+	
+	void Update () {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            up = true;
+        }
+        else
+        {
+            up = false;
+        }
+	}
+
+    private void FixedUpdate()
+    {
+        if (up && rb.velocity.x < 5 && rb.velocity.y < 5)
+        {
+            rb.AddForce(new Vector3(20, 0, 0));
+        }
+    }
+}
