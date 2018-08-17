c905f703323b4b35d248e5a08f2aa86f1a622c27
Dd <jedendaniel@gmail.com>
2018-08-17 16:09:08 +0200

    Add race end

--- Rally champions Tokio drift/Assets/Scripts/Car.cs
+++ Rally champions Tokio drift/Assets/Scripts/Car.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Text;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Car : MonoBehaviour {
     
@@ -8,9 +10,26 @@ public class Car : MonoBehaviour {
     float horizontalInput;
     Rigidbody rb;
     public InputSettings inputSettings;
+    public Text lapText;
+    public Text endText;
+    int lap;
+    [HideInInspector]
+    public int laps;
+    Camera[] cameras;
+    public string name;
+    public Canvas uiCanvas;
 
-	void Start () {
+    public int Lap
+    {
+        get
+        {
+            return lap;
+        }
+    }
+
+    void Start () {
         rb = gameObject.GetComponent<Rigidbody>();
+        cameras = GetComponentsInChildren<Camera>();
     }
 	
 	void Update () {
@@ -34,6 +53,50 @@ public class Car : MonoBehaviour {
         if (Mathf.Abs(rb.velocity.x) > 1f || Mathf.Abs(rb.velocity.z) > 1f)
         {
             rb.transform.Rotate(new Vector3(0, horizontalInput, 0));
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Lap+")
+        {
+            if (transform.position.z < other.transform.position.z)
+                lap++;
+            else
+                lap--;
+            lapText.text = "Lap: " + Lap.ToString() + "/" + laps.ToString();
+        }
+    }
+
+    public void EndRace(int place)
+    {
+        endText.gameObject.SetActive(true);
+        StringBuilder message = new StringBuilder();
+        message.Append(place);
+        message.Append(GetNumberSuffix(place));
+        message.Append(" place!");
+        message.AppendLine();
+        message.Append("Good job!");
+        endText.text = message.ToString();
+        MeshRenderer rend;
+        rend = GetComponent<MeshRenderer>();
+        rend.enabled = false;
+    }
+
+    public static string GetNumberSuffix(int number)
+    {
+        if (number == 1) return "st";
+        if (number == 2) return "nd";
+        if (number == 3) return "rd";
+        return "th";
+    }
+
+    private void OnDisable()
+    {
+        uiCanvas.enabled = false;
+        foreach(Camera cam in cameras)
+        {
+            cam.enabled = false;
         }
     }
 }
--- Rally champions Tokio drift/Assets/Scripts/GameManager.cs
+++ Rally champions Tokio drift/Assets/Scripts/GameManager.cs
@@ -1,25 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Text;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
 
     public List<Car> cars;
+    List<Car> carsLeft;
     float verticalParts = 1;
     float horizontalParts = 1;
+    int laps = 1;
+    Dictionary<int, Car> places = new Dictionary<int, Car>();
+    int place = 1;
+    public Text EndResults;
+    public Camera mainCamera;
+
+    bool gameFinished;
 
     void Start ()
     {
+        carsLeft = new List<Car>(cars);
         GetScreenParts();
-        if (cars.Count != 1)
-        {
-            SplitScreen();
-        }
-	}
+        SplitScreen();
+    }
 	
 	void Update () {
-		
-	}
+        CheckCarsProgress();
+    }
 
     void GetScreenParts()
     {
@@ -43,6 +51,8 @@ public class GameManager : MonoBehaviour {
         float h = 0;
         foreach(Car car in cars)
         {
+            car.laps = laps;
+            car.lapText.text = "Lap: 0/" + laps.ToString();
             Camera[] carCameras = car.GetComponentsInChildren<Camera>();
             foreach (Camera camera in carCameras)
             {
@@ -59,5 +69,45 @@ public class GameManager : MonoBehaviour {
             else
                 v += 1;
         }
+    }
+
+    void CheckCarsProgress()
+    {
+        if (gameFinished) return;
+        List<Car> finishedCars = new List<Car>();
+        foreach (Car car in carsLeft)
+        {
+            if (car.Lap == laps)
+            {
+                finishedCars.Add(car);
+                places.Add(place, car);
+                car.EndRace(place);
+            }
+        }
+        foreach(Car car in finishedCars)
+        {
+            place++;
+            carsLeft.Remove(car);
+        }
+        if (carsLeft.Count == 0) EndGame();
+    }
+
+    void EndGame()
+    {
+        StringBuilder text = new StringBuilder();
+        text.AppendLine();
+        foreach(KeyValuePair<int,Car> place in places)
+        {
+            text.Append(place.Key.ToString());
+            text.Append(". ");
+            text.Append(place.Value.name);
+            text.AppendLine();
+            place.Value.enabled = false;
+        }
+        mainCamera.rect = new Rect(0, 0, 1, 1);
+        EndResults.transform.parent.gameObject.SetActive(true);
+        EndResults.enabled = true;
+        EndResults.text += text.ToString();
+        gameFinished = true;
     }
 }
